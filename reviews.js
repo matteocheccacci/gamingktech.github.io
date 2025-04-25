223b56c26282aac6a0266f2f3937457c435b37e7
Matteo Checcacci <matteocheccacci@gmail.com>
2025-04-25 18:33:35 +0200

    prova

---
+++ reviews.js
@@ -0,0 +1,42 @@
+const reviews = [
+    {
+        title: "The Last of Us Part III: Un Capolavoro Emozionante",
+        date: "2025-04-25",
+        excerpt: "Un'esperienza narrativa intensa e personaggi indimenticabili. La conclusione perfetta per la saga?",
+        gamename: "the-last-of-us-part-iii",
+        backgroundImage: "the-last-of-us-part-iii/copertina.jpg", // Percorso relativo direttamente alla cartella del gioco
+        link: "the-last-of-us-part-iii/index.html" // Percorso relativo direttamente alla cartella del gioco
+    },
+    {
+        title: "Cyberpunk 2077: Phantom Liberty - Redenzione a Night City",
+        date: "2025-04-10",
+        excerpt: "L'espansione che tutti aspettavano. Nuova storia, personaggi e un'atmosfera ancora più coinvolgente.",
+        gamename: "cyberpunk-2077-phantom-liberty",
+        backgroundImage: "cyberpunk-2077-phantom-liberty/copertina.jpg",
+        link: "cyberpunk-2077-phantom-liberty/index.html"
+    },
+    {
+        title: "Baldur's Gate 3: Un RPG Epico che Ridefinisce il Genere",
+        date: "2025-03-15",
+        excerpt: "Libertà di scelta senza precedenti, combattimenti tattici profondi e una storia che cattura dall'inizio alla fine.",
+        gamename: "baldurs-gate-3",
+        backgroundImage: "baldurs-gate-3/copertina.jpg",
+        link: "baldurs-gate-3/index.html"
+    },
+    {
+        title: "Starfield: Esplorando l'Infinito e Oltre",
+        date: "2025-02-28",
+        excerpt: "Un'avventura spaziale ambiziosa con una vastità di mondi da scoprire e segreti da svelare.",
+        gamename: "starfield",
+        backgroundImage: "starfield/copertina.jpg",
+        link: "starfield/index.html"
+    },
+    {
+        title: "Hogwarts Legacy: La Magia Prende Vita",
+        date: "2025-01-20",
+        excerpt: "Un'immersione nel mondo di Harry Potter come non l'avete mai vista. Esplorazione, magia e misteri vi attendono.",
+        gamename: "hogwarts-legacy",
+        backgroundImage: "hogwarts-legacy/copertina.jpg",
+        link: "hogwarts-legacy/index.html"
+    }
+];
